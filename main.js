3debc107f4ec9f23fbe6e2a287b176bd55e1757a
Hazem agil <21099@ppu.edu.ps>
2024-01-15 23:09:59 +0200

    my task

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var exchangeRates = {
+        'dollar': 0.26, 
+        'dinar': 0.20,  
+        'nis': 1       
+    };
+
+    var form = document.querySelector('.registerForm');
+    var resultParagraph = document.querySelector('.result');
+
+    function convertCurrency(amount, currency) {
+        return amount * exchangeRates[currency];
+    }
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); 
+        var amount = form.querySelector('input[name="amount"]').value;
+        var currency = form.querySelector('select[name="exchange"]').value;
+        var convertedAmount = convertCurrency(amount, currency);
+        resultParagraph.textContent = convertedAmount;
+    });
+});
